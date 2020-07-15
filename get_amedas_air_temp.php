13877b90406fcf2c96cba6cb6603543208374563
horoiwa <horoiwa.yasuhiro@corporate.co.jp>
2020-07-15 10:56:56 +0900

    replese時に計算量がずれる部分を修正

--- get_amedas_air_temp.php
+++ get_amedas_air_temp.php
@@ -52,20 +52,20 @@ if($which==0)$which=24;
 $now=0;            //時刻ではない
 /* 繰り返しを行いながら、個々の項目を解析する。 */
 foreach( $_rawData as $_rawBlock ) {
-   //初期化
-   //$time=null;
-   $temp=null;	//気温
+    //初期化
+    //$time=null;
+    $temp=null;	//気温
    
-   //now=0:$which=1        //ひとつずれてる
-   if($now==($which)){
-     $_rawBlock = explode( "</td>", $_rawBlock );//2つのデータを配列に格納...余計に1つ要素が出る..
-     $str="";
-     $null_count=0;
-     $num_element=count($_rawBlock)-1;
-     for($j=0;$j<$num_element;$j++){
-        $_rawBlock[$j]=strip_tags($_rawBlock[$j]);    //余計なタグを除去
-        $_rawBlock[$j] = trim( $_rawBlock[$j] );    //空白を除去
-        //$_rawBlock[$j] = str_replace('x', '', $_rawBlock[$j])
+    //now=0:$which=1        //ひとつずれてる
+    if($now==($which)){
+        $_rawBlock = explode( "</td>", $_rawBlock );//2つのデータを配列に格納...余計に1つ要素が出る..
+        $str="";
+        $null_count=0;
+        $num_element=count($_rawBlock)-1;
+        for($j=0;$j<$num_element;$j++){
+            $_rawBlock[$j]=strip_tags($_rawBlock[$j]);    //余計なタグを除去
+            $_rawBlock[$j] = trim( $_rawBlock[$j] );    //空白を除去
+            //$_rawBlock[$j] = str_replace('x', '', $_rawBlock[$j])
 
 
         //echo $_rawBlock[$j];
@@ -76,93 +76,93 @@ foreach( $_rawData as $_rawBlock ) {
        $null_count++;
     }
     if($j==0){
-       //時間
-       //$time=$_rawBlock[$j];
-       //if($time==24)$time=0;    //24時 => 0時
+        //時間
+        //$time=$_rawBlock[$j];
+        //if($time==24)$time=0;    //24時 => 0時
     }else if($j==1){
-       //気温（℃）
-       $temp=$_rawBlock[$j];
-       $str.=$temp;
+        //気温（℃）
+        $temp=$_rawBlock[$j];
+        $str.=$temp;
     }
-     }
-     if($null_count==$num_element-1){
-         //echo "null!!";
-     }
+    }
+    if($null_count==$num_element-1){
+        //echo "null!!";
+    }
     $sc_time = $_rawBlock[0];
     $sc_temp = $_rawBlock[1];
     $sc_rain = $_rawBlock[2];
     $sc_wind = $_rawBlock[3];
     $sc_wspd = $_rawBlock[4];
     $sc_sun  = $_rawBlock[5];
+    //24時→0時として登録する
+    if ($sc_time === "24:00:00") {
+        $sc_time = "00:00:00";
+    }
     $sc_time = $sc_time . ":00:00";
     $sc_date = date('Y-m-d');
 
 
-     //ＭｙＳＱＬへ接続(DB_HOST, DB_USER, DB_PASS)
-     $mysqli = new mysqli ($host_name, $user_name, $db_password, $db_name);
-     if ($mysqli->connect_error) {
-         echo $mysqli->connect_error;
-         exit();
-     } else {
-         $mysqli->set_charset("utf8");
-     }
+    //ＭｙＳＱＬへ接続(DB_HOST, DB_USER, DB_PASS)
+    $mysqli = new mysqli ($host_name, $user_name, $db_password, $db_name);
+    if ($mysqli->connect_error) {
+        echo $mysqli->connect_error;
+        exit();
+    } else {
+        $mysqli->set_charset("utf8");
+    }
 
 
-     // mysql構文1　最新の総雨量データを取得
-     $sql = 'select rain_total from ksfoods.area_info order by day desc, time desc limit 1;';
-     $rain_total_row = $mysqli->query($sql);
-     if (!$rain_total_row) {
-         die('select fault'.mysql_error());
-     }
-     $rain_total_row = $rain_total_row->fetch_array();
-     $rain_total_before = $rain_total_row[0];
-     echo "直前の総雨量:". $rain_total_before . "\n";                              // 直前の総雨量
+    // mysql構文1　最新の総雨量データを取得
+    $sql = 'select rain_total from ksfoods.area_info where time <> "' . $sc_time . '" order by day desc, time desc limit 1;';
+    $rain_total_row = $mysqli->query($sql);
+    if (!$rain_total_row) {
+        die('select fault'.mysql_error());
+    }
+    $rain_total_row = $rain_total_row->fetch_array();
+    $rain_total_before = $rain_total_row[0];
+    echo "直前の総雨量:". $rain_total_before . "\n";                              // 直前の総雨量
 
-     // mysql構文2　当日の降水量データを取得
-     $sql = "select sum(rain_hour) from ksfoods.area_info where day=date(now());";
-     $rain_todayall_row = $mysqli->query($sql);
-     if (!$rain_todayall_row) {
-         die('select fault'.mysql_error());
-     }
-     $rain_todayall_row = $rain_todayall_row->fetch_array();
-     $rain_todayall = $rain_todayall_row[0];
-     $rain_todayall = (float)$rain_todayall + (float)$sc_rain;    //最新の雨量を加算
-     echo "当日降水量:". $rain_todayall . "\n";                                  // 当日降水量
+    // mysql構文2　当日の降水量データを取得
+    $sql = 'select sum(rain_hour) from ksfoods.area_info where day=date(now()) and time <> "' . $sc_time . '";';
+    $rain_todayall_row = $mysqli->query($sql);
+    if (!$rain_todayall_row) {
+        die('select fault'.mysql_error());
+    }
+    $rain_todayall_row = $rain_todayall_row->fetch_array();
+    $rain_todayall = $rain_todayall_row[0];
+    $rain_todayall = (float)$rain_todayall + (float)$sc_rain;    //最新の雨量を加算
+    echo "当日降水量:". $rain_todayall . "\n";                                  // 当日降水量
 
 
-     // mysql構文3　総雨量確認と計算リセット用(直前4時間分の降雨量データを取得)
-     $sql = 'select sum(rain_hour) from (select rain_hour from ksfoods.area_info order by day desc, time desc limit 4) as subt;';
-     $rain_total_row = $mysqli->query($sql);
-     if (!$rain_total_row) {
-         die('select fault'.mysql_error());
-     }
-     $rain_total_row = $rain_total_row->fetch_array();
-     $rain_total_check = $rain_total_row[0];
-     $rain_total = (float)$rain_total_before + (float)$sc_rain;   //総雨量に最新データを加算
-     if ((float)$rain_total_check == 0.0) {                       //取得した雨量がゼロなら総雨量リセット
-         $rain_total = $sc_rain;
-     }
-     echo "登録される総雨量:". $rain_total. "\n";
+    // mysql構文3　総雨量確認と計算リセット用(直前4時間分の降雨量データを取得)
+    $sql = 'select sum(rain_hour) from (select rain_hour from ksfoods.area_info order by day desc, time desc limit 4) as subt;';
+    $rain_total_row = $mysqli->query($sql);
+    if (!$rain_total_row) {
+        die('select fault'.mysql_error());
+    }
+    $rain_total_row = $rain_total_row->fetch_array();
+    $rain_total_check = $rain_total_row[0];
+    $rain_total = (float)$rain_total_before + (float)$sc_rain;   //総雨量に最新データを加算
+    if ((float)$rain_total_check == 0.0) {                       //取得した雨量がゼロなら総雨量リセット
+        $rain_total = $sc_rain;
+    }
+    echo "登録される総雨量:". $rain_total. "\n";
 
-     //24時→0時として登録する
-     if ($sc_time === "24:00:00") {
-         $sc_time = "00:00:00";
-     }
 
-     //登録データ確認
-     $sc_date = '"' . $sc_date . '"';
-     $sc_time = '"' . $sc_time . '"';
+    //登録データ確認
+    $sc_date = '"' . $sc_date . '"';
+    $sc_time = '"' . $sc_time . '"';
 
-     //mysql構文4 データ登録用
-     $sql = "replace into area_info values ( " . $area_no . ", " . $sc_date . ", " . $sc_time . ", " . (float)$sc_temp . ", " . (float)$sc_rain . ", " . (float)$rain_todayall . ", " . (float)$rain_total . ");";
+    //mysql構文4 データ登録用
+    $sql = "replace into area_info values ( " . $area_no . ", " . $sc_date . ", " . $sc_time . ", " . (float)$sc_temp . ", " . (float)$sc_rain . ", " . (float)$rain_todayall . ", " . (float)$rain_total . ");";
 
-     $mysqli_result = $mysqli->query($sql);
-     if (!$mysqli_result) {
-         die('insert fault'.mysql_error() . "\n");
-     }
+    $mysqli_result = $mysqli->query($sql);
+    if (!$mysqli_result) {
+        die('insert fault'.mysql_error() . "\n");
+    }
 
-     $mysqli->close();        //DB.close();
-   }
-   $now++;
+    $mysqli->close();        //DB.close();
+    }
+    $now++;
 }
 ?>
